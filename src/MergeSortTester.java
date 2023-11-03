21ed5659b32b5a400592dc4a470b8216af6a1b18
BigBooleanJohn <119363172+BigBooleanJohn@users.noreply.github.com>
2023-11-02 22:36:34 -0500

    Update MergeSortTester.java with my tests

--- src/MergeSortTester.java
+++ src/MergeSortTester.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
 /**
  * Tests of MergeSort.
  */
@@ -11,5 +15,29 @@ public class MergeSortTester extends SortTester {
     super();
     this.sorter = MergeSort.SORTER;
   } // MergeSortTester()
+
+  @Test
+  public void orderedStringTest() {
+    String[] original = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    String[] expected = original.clone();
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+  @Test
+  public void reverseOrderedStringTest() {
+    String[] original = { "foxtrot", "delta", "charlie", "bravo", "alpha" };
+    String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+  @Test
+  public void similarStringTest() {
+    String[] original = { "delte", "dalta", "dolto" };
+    String[] expected = { "dalta", "delte", "dolto" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
 
 } // class MergeSortTester
