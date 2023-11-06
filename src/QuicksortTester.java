95efe8c56af8fc195deff76fb05bb1fab7b9f11a
BigBooleanJohn <119363172+BigBooleanJohn@users.noreply.github.com>
2023-11-06 11:37:56 -0600

    Update QuicksortTester.java

--- src/QuicksortTester.java
+++ src/QuicksortTester.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
 /**
  * Tests of Quicksort.
  */
@@ -12,4 +16,27 @@ public class QuicksortTester extends SortTester {
     this.sorter = Quicksort.SORTER;
   } // QuicksortTester()
 
+  @Test
+  public void similarStringTest() {
+    String[] original = { "a", "c", "b", "d", "f", "e" };
+    String[] expected = { "a", "b", "c", "d", "e", "f" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+  @Test
+  public void reverseOrderedStringTest() {
+    String[] original = { "foxtrot", "delta", "charlie", "bravo", "alpha" };
+    String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // reverseOrderedStringTest
+
+  @Test
+  public void orderedStringTest() {
+    String[] original = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    String[] expected = original.clone();
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
 } // class QuicksortTester
